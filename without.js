9453acc3ce7390f95e07ad78d26aae51ea298ac5
Brandon Lecky <dreamxinxcode@gmail.com>
2020-07-21 21:54:07 +0000

    create new array without said items

---
+++ without.js
@@ -0,0 +1,46 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+
+const assertArraysEqual = function(arr, arr2) {
+  if (arr.length !== arr2.length) {
+    // Using return to exit (maybe there's a better way..)
+    return console.log(`🛑🛑🛑 Assertion Failed: [${arr}] !== [${arr2}]`);
+  } else {
+    for (let x = 0; x < arr.length; x++) {
+      if (arr[x] !== arr2[x]) {
+        return console.log(`🛑🛑🛑 Assertion Failed: [${arr}] !== [${arr2}]`);
+      }
+    }
+  }
+  return console.log(`✅✅✅ Assertion Passed: [${arr}] === [${arr2}]`);
+};
+
+
+const without = function(source, itemsToRemove) {
+  let subArr = [];
+  for (let i = 0; i < source.length; i++) {
+    if (!itemsToRemove.includes(source[i])) {
+      subArr.push(source[i]);
+    }
+  }
+  console.log(subArr);
+};
+
+without([1, 2, 3], [1]); // => [2, 3]
+without(["1", "2", "3"], [1, 2, "3"]); // => ["1", "2"]
+without(["1", "2", "3"], [1, 2, "3"]) // => ["1", "2"]
+without(["hello", "world", "3"], ['hello', 2, "3"]); // => ["1", "2"]
+
+
+
+
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
