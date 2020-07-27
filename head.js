e9129628eff6abac0566066986a7bd19e72b7098
Brandon Lecky <dreamxinxcode@gmail.com>
2020-07-27 13:13:53 +0000

    refactord a few files to ude require and export

--- head.js
+++ head.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const head = function(arr) {
   if (arr.length === 0) {
@@ -14,7 +8,4 @@ const head = function(arr) {
   }
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), 5);
-assertEqual(head(["Hello"]), "Hello");
+module.exports = head;
